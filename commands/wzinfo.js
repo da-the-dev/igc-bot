c4b2099f04602b1d7671cc022029675e6f12d810
da-the-dev <leshiktk@me.com>
2021-06-02 14:09:51 +0300

    Renamed

--- commands/wzinfo.js
+++ commands/wzinfo.js
@@ -3,7 +3,7 @@ const axios = require('axios').default
 const jsdom = require("jsdom")
 const { db } = require('../utility')
 const { DBUser } = db
-
+const constants = require('../constants.json')
 
 module.exports =
     /**
@@ -30,22 +30,19 @@ module.exports =
 
                 const level = `\`${dom.window.document.getElementsByClassName('highlighted-stat')[0].children[1].children[0].textContent.replace('Level ', '')}\``
                 const prestige = `\`${dom.window.document.getElementsByClassName('highlighted-stat')[0].children[1].children[1].textContent.trim().replace('Prestige ', '')}\``
-                const kd = `\`${stats[22].children[1].textContent}\``
-                const kills = `\`${stats[20].children[1].textContent}\``
-                const deaths = `\`${stats[21].children[1].textContent}\``
+                const kd = `\`${stats[22].children[1].textContent} (${stats[22].children[2].children[0].textContent})\``
+                const kills = `\`${stats[20].children[1].textContent} (${stats[20].children[2].children[0].textContent})\``
+                const deaths = `\`${stats[21].children[1].textContent} (${stats[21].children[2].children[0].textContent})\``
                 const matches = `\`${dom.window.document.getElementsByClassName('matches')[1].textContent.trim().replace(' Matches', '')}\``
-                const wins = `\`${stats[16].children[1].textContent}\``
-                const top5 = `\`${stats[17].children[1].textContent}\``
-                const top10 = `\`${stats[18].children[1].textContent}\``
-                const top25 = `\`${stats[19].children[1].textContent}\``
-                const ppm = `\`${stats[26].children[1].textContent}\``
-                const winsPercent = `\`${stats[29].children[1].textContent}\``
+                const wins = `\`${stats[16].children[1].textContent} (${stats[16].children[2].children[0].textContent})\``
+                const top5 = `\`${stats[17].children[1].textContent} (${stats[17].children[2].children[0].textContent})\``
+                // const top10 = `\`${stats[18].children[1].textContent} (${stats[18].children[2].children[0].textContent})\``
+                const top25 = `\`${stats[19].children[1].textContent} (${stats[19].children[2].children[0].textContent})\``
 
-                const pKills = `\`${stats[41].children[1].textContent}\``
+                const pKills = `\`${stats[41].children[1].textContent} (${stats[41].children[2].children[0].textContent})\``
                 const pMatches = `\`${dom.window.document.getElementsByClassName('matches')[2].textContent.trim().replace(' Matches', '')}\``
-                const pKD = `\`${stats[43].children[1].textContent}\``
-                const pDeaths = `\`${stats[42].children[1].textContent}\``
-                const pPpm = `\`${stats[47].children[1].textContent}\``
+                const pKD = `\`${stats[43].children[1].textContent} (${stats[43].children[2].children[0].textContent})\``
+                const pDeaths = `\`${stats[42].children[1].textContent} (${stats[42].children[2].children[0].textContent})\``
 
                 const embed = new MessageEmbed({
                     "title": " Обновление статистики Warzone",
@@ -54,19 +51,42 @@ module.exports =
                     "fields": [
                         {
                             "name": "**Батл рояль**",
-                            "value": `> Уровень: ${level}\n> Престиж: ${prestige}\n> K/D: ${kd}\n> Убийства: ${kills}\n> Смерти: ${deaths}\n> Матчей: ${matches}\n> Победы: ${wins}\n> Топ 5: ${top5}\n> Топ 10: ${top10}\n> Топ 25: ${top25}\n> Очки/мин: ${ppm}\n> Победы %: ${winsPercent}`,
+                            "value": `> Уровень: ${level}\n> Престиж: ${prestige}\n> K/D: ${kd}\n> Убийства: ${kills}\n> Смерти: ${deaths}\n> Матчей: ${matches}\n> Победы: ${wins}\n> Топ 5: ${top5}\n> Топ 10: ${'1'}\n> Топ 25: ${top25}`,
                             "inline": true
                         },
                         {
                             "name": "**Добыча**",
-                            "value": `> Убийства: ${pKills}\n> Матчей: ${pMatches}\n> K/D: ${pKD}\n> Смертей: ${pDeaths}\n> Очки/мин: ${pPpm}\n`,
+                            "value": `> Убийства: ${pKills}\n> Матчей: ${pMatches}\n> K/D: ${pKD}\n> Смертей: ${pDeaths}`,
                             "inline": true
                         }
                     ]
                 })
                     .setAuthor(msg.author.tag, msg.author.displayAvatarURL({ dynamic: true }))
                     .setFooter('Авторство бота snipertf2#6625', 'https://cdn.discordapp.com/avatars/315339158912761856/6dc8a2b8f4f20f84bc099e1dfda20d3f.webp')
+                    .setImage('https://i.stack.imgur.com/Fzh0w.png')
+                    .setThumbnail('https://media.discordapp.net/attachments/849266054051528725/849584645040635914/20210602_124545.jpg?width=1138&height=1138')
                 msg.channel.send(embed)
+
+                const wzkds = [
+                    constants.roles.wzkd1,
+                    constants.roles.wzkd2,
+                    constants.roles.wzkd3
+                ]
+
+                const numKD = Number(stats[22].children[1].textContent)
+                if(numKD >= 1 && numKD < 2 && !msg.member.roles.cache.get(constants.roles.wzkd1)) {
+                    msg.member.roles.remove(wzkds)
+                    msg.member.roles.add(constants.roles.wzkd1)
+                }
+                if(numKD >= 2 && numKD < 3 && !msg.member.roles.cache.get(constants.roles.wzkd2)) {
+                    msg.member.roles.remove(wzkds)
+                    msg.member.roles.add(constants.roles.wzkd2)
+                }
+                if(numKD >= 3 && !msg.member.roles.cache.get(constants.roles.wzkd3)) {
+                    msg.member.roles.remove(wzkds)
+                    msg.member.roles.add(constants.roles.wzkd3)
+                }
+
             })
             .catch(err => {
                 if(err.response.status == '404')
