ca6ef5e1b9a6146128ee31eddf38e5d956a16d7d
da-the-dev <leshiktk@me.com>
2021-06-05 22:08:48 +0300

    Added role restrictions

--- utility/streamerTracker.js
+++ utility/streamerTracker.js
@@ -6,7 +6,12 @@ const constants = require('../constants.json')
  * @param {Presence} presence 
  */
 module.exports = presence => {
-    if(presence.activities.find(a => a.type == "STREAMING"))
+    if(presence.activities.find(a => a.type == "STREAMING")
+        && presence.member.roles.cache.find(r => [
+            "355307608648843264",
+            "777264089797754901",
+            "703317566734598166"
+        ].includes(r.id)))
         presence.member.roles.add(constants.roles.live)
     else
         if(presence.member.roles.cache.has(constants.roles.live))
