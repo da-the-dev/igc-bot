2e2c6d1bf9286d50bd16d8173688a8ac8ee760d7
da-the-dev <leshiktk@me.com>
2021-06-13 14:20:00 +0300

    Moved MW stuff in its folder

--- commands/mw/mwclear.js
+++ commands/mw/mwclear.js
@@ -1,13 +1,13 @@
 const { Message, Client } = require('discord.js')
-const { tracker } = require('../utility')
+const { tracker } = require('../../utility')
 
 module.exports =
     /**
     * @param {Array<string>} args Command argument
     * @param {Message} msg Discord message object
     * @param {Client} client Discord client object
-    * @description Usage: .mwreg <usertag> <platform>
+    * @description Usage: .mwclear
     */
     async (args, msg, client) => {
-        tracker.reg(msg, args, 'modern-warfare')
+        tracker.clear(msg, 'modern-warfare')
     }
--- commands/mw/mwreg.js
+++ commands/mw/mwreg.js
@@ -1,13 +1,13 @@
 const { Message, Client } = require('discord.js')
-const { tracker } = require('../utility')
+const { tracker } = require('../../utility')
 
 module.exports =
     /**
     * @param {Array<string>} args Command argument
     * @param {Message} msg Discord message object
     * @param {Client} client Discord client object
-    * @description Usage: .mwclear
+    * @description Usage: .mwreg <usertag> <platform>
     */
     async (args, msg, client) => {
-        tracker.clear(msg, 'modern-warfare')
+        tracker.reg(msg, args, 'modern-warfare')
     }
